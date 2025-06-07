fb9d5252e1b434c6da37418e6968815965c39b4e
nahomwores <nahomwores7@gmail.com>
2025-06-07 13:42:01 +0300

    built a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show, useBreakpointValue  } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
   return (
-    <>
-     <Button>Click me</Button>
-    </>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // For small screens
+        lg: `"nav nav" "aside main"`, // Adjusted for larger screens
+      }}
+    >
+      
+      <GridItem area="nav">
+       <NavBar/>
+      </GridItem>
+      <Show when={isLargeScreen}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
